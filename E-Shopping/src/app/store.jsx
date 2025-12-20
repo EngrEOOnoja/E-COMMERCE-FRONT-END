2d843cadaa208429d743850d6e6d457269115720
EngrEOOnoja <onojaemmanuel193@gmail.com>
2025-12-20 02:43:19 +0100

    change product

--- E-Shopping/src/app/store.jsx
+++ E-Shopping/src/app/store.jsx
@@ -1,6 +1,6 @@
 // src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import { productsApi } from '../products/ProductsApi.js';
+import { productsApi } from '../products/ProductsApi.jsx';
 import cartReducer from '../cart/cartSlice';
 
 export const store = configureStore({
